27e53912fd8cf2c4cc590add4c064dd23008a08f
Jan Sattler <jan1081@pm.me>
2024-02-03 18:26:21 +0100

    init

---
+++ setupj/__init__.py
@@ -0,0 +1,78 @@
+def notebook_autoreload():
+    from IPython import get_ipython
+
+    """Import this function in a jupyter notebook and run it to enable autoreload of modules"""
+    get_ipython().run_line_magic("load_ext", "autoreload")
+    get_ipython().run_line_magic("autoreload", "2")
+
+
+def notebook_transparent():
+    from IPython import get_ipython
+
+    """Import this function in a jupyter notebook and run it to set the background to transparent"""
+    get_ipython().run_cell_magic(
+        "html",
+        "",  # NOTE not a specific cell
+        """
+        <style>
+        .cell-output-ipywidget-background {background-color: transparent !important;}
+        .jp-OutputArea-output {background-color: transparent;}
+        </style>
+        """,
+    )
+
+
+class _PlotlyConfig:
+    def default(self):
+        """Do not change the default plotly renderer of fig.show()"""
+        self._set_renderer(None)
+
+    def minimal_filesize(self):
+        """Figures are not saved inside the notebook, works only with fig.show()"""
+        self._set_renderer("mokeypatch")
+
+    def github_svg(self):
+        """Plots are displayed on GitHub and in HTML exports without creating large files."""
+        self._set_renderer("svg")
+
+    def github_png(self):
+        self._set_renderer("png")
+
+    def notebook(self):
+        self._set_renderer("notebook")
+
+    def vscode(self):
+        self._set_renderer("vscode")
+
+    def custom(self, renderer: str):
+        """
+        "browser": opens plots in a browser window
+        For more options see: https://plotly.com/python/renderers/
+        """
+        self._set_renderer(renderer)
+
+    def _set_renderer(self, renderer: str):
+        import plotly.io as pio
+        from IPython.display import display
+        from plotly.basedatatypes import BaseFigure
+
+        if renderer == "mokeypatch":
+
+            def show(self, *args, **kwargs):
+                """This function monkey patches (modifies at runtime) behavior of fig.show()."""
+                display(
+                    self,
+                    # raw=True,
+                )
+
+        else:
+            pio.renderers.default = renderer
+
+            def show(self, *args, **kwargs):
+                "Default implementation of fig.show()"
+                return pio.show(self, *args, **kwargs)
+
+        BaseFigure.show = show
+
+
+notebook_renderplotly = _PlotlyConfig()
